4fd4e59235ed1891f9a584c75950d25c61687205
David Monson <david@808loud.com>
2017-01-17 00:32:44 -0600

    update content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,9 @@
+$('body').scrollspy({ target: '.navbar' })
 
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 1) {
+    $('.navbar').addClass('shrink');
+  } else {
+    $('.navbar').removeClass('shrink');
+  }
+});
