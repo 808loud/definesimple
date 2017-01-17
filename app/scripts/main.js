732794c4377ac45716a7ee092b1f953f4c87c5b7
David Monson <david@808loud.com>
2017-01-17 01:59:15 -0600

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+new WOW({ offset: 70 }).init();
+
+$('body').scrollspy({ target: '.navbar' })
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 1) {
+    $('.navbar').addClass('shrink');
+  } else {
+    $('.navbar').removeClass('shrink');
+  }
+});
